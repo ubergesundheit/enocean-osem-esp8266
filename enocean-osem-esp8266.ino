c3c6b5a94d41acd8f4a8d36ba60d6a05dec96dd0
ubergesundheit <ubergesundheit@googlemail.com>
2017-11-18 23:06:00 +0100

    cleanup, add only verbose logging in debug mode

--- enocean-osem-esp8266.ino
+++ enocean-osem-esp8266.ino
@@ -7,6 +7,9 @@
 
 #include <WiFiClientSecure.h>
 
+// Uncomment to enable verbose decoding information
+// #define ENABLE_DEBUG
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 /////// Wifi Settings ///////
 char ssid[] = SECRET_SSID;
@@ -26,6 +29,14 @@ int statusCode = 0;
 byte rx_buffer[256];
 char url_buffer[256];
 char data_buffer[256];
+
+#ifdef ENABLE_DEBUG
+#define DEBUGln(str) Serial.println(str)
+#define DEBUG_p(str) Serial.print(str)
+#else
+#define DEBUGln(str)
+#define DEBUG(str)
+#endif
 
 void setup() {
   Serial.begin(115200);
@@ -73,31 +84,31 @@ uint8_t decodeRxBuffer() {
 
   // volt
   float volt = 0.0258039 * rx_buffer[1];
-  Serial.println(F("decoded volt"));
-  Serial.println(volt);
+  DEBUGln(F("decoded volt"));
+  DEBUGln(volt);
 
   // humidity
   float hum = (float)rx_buffer[2] / 2.5;
-  Serial.println(F("decoded hum"));
-  Serial.println(hum);
+  DEBUGln(F("decoded hum"));
+  DEBUGln(hum);
 
   // temperature
   float temp = 0.32 * (float)rx_buffer[3] - 20.0;
-  Serial.println(F("decoded temp"));
-  Serial.println(temp);
+  DEBUGln(F("decoded temp"));
+  DEBUGln(temp);
 
-  Serial.println(F("determining device"));
+  DEBUGln(F("determining device"));
   if (rx_buffer[5] == OUTDOOR_ADDRESS_BYTE_0 &&
       rx_buffer[6] == OUTDOOR_ADDRESS_BYTE_1 &&
       rx_buffer[7] == OUTDOOR_ADDRESS_BYTE_2 &&
       rx_buffer[8] == OUTDOOR_ADDRESS_BYTE_3) {
-    Serial.println(F("is outdoor"));
+    DEBUGln(F("is outdoor"));
     sprintf(url_buffer, url_buffer_template, OUTDOOR_SENSEBOX_ID);
-    Serial.println(url_buffer);
+    DEBUGln(url_buffer);
     sprintf(data_buffer, data_buffer_template, OUTDOOR_VOLT_ID,
             String(volt, 1).c_str(), OUTDOOR_TEMP_ID, String(temp, 1).c_str(),
             OUTDOOR_HUMI_ID, String(hum, 1).c_str());
-    Serial.println(data_buffer);
+    DEBUGln(data_buffer);
 
     return 1;
   }
@@ -106,13 +117,13 @@ uint8_t decodeRxBuffer() {
       rx_buffer[6] == INDOOR_ADDRESS_BYTE_1 &&
       rx_buffer[7] == INDOOR_ADDRESS_BYTE_2 &&
       rx_buffer[8] == INDOOR_ADDRESS_BYTE_3) {
-    Serial.println(F("is indoor"));
+    DEBUGln(F("is indoor"));
     sprintf(url_buffer, url_buffer_template, INDOOR_SENSEBOX_ID);
-    Serial.println(url_buffer);
+    DEBUGln(url_buffer);
     sprintf(data_buffer, data_buffer_template, INDOOR_VOLT_ID,
             String(volt, 1).c_str(), INDOOR_TEMP_ID, String(temp, 1).c_str(),
             INDOOR_HUMI_ID, String(hum, 1).c_str());
-    Serial.println(data_buffer);
+    DEBUGln(data_buffer);
 
     return 1;
   }
@@ -124,7 +135,7 @@ void loop() {
   while (swSer.available() > 0) {
     // wait for sync byte
     if (swSer.read() == 0x55) {
-      Serial.println("0: 55");
+      DEBUGln("0: 55");
       // read DATA length
       byte b_0 = swSer.read();
       byte b_1 = swSer.read();
@@ -132,38 +143,29 @@ void loop() {
       // read OPTIONAL_DATA length
       uint8_t OPTIONAL_DATAlen = swSer.read();
 
-      Serial.print("1: ");
-      Serial.println(b_0, HEX);
-      Serial.print("2: ");
-      Serial.println(b_1, HEX);
-      Serial.print("3: ");
-      Serial.println(OPTIONAL_DATAlen, HEX);
+      DEBUG("1: ");
+      DEBUGln(b_0, HEX);
+      DEBUG("2: ");
+      DEBUGln(b_1, HEX);
+      DEBUG("3: ");
+      DEBUGln(OPTIONAL_DATAlen, HEX);
 
-      Serial.print("datalen:");
-      Serial.println(DATAlen);
+      DEBUG("datalen:");
+      DEBUGln(DATAlen);
 
-      Serial.print("optionallen:");
-      Serial.println(OPTIONAL_DATAlen);
+      DEBUG("optionallen:");
+      DEBUGln(OPTIONAL_DATAlen);
 
       // read PACKET_TYPE
       if (swSer.read() != 0x01) {
         continue;
       }
-      Serial.println("4: 1");
+      DEBUGln("4: 1");
 
       // read (and discard) CRC8_HEADER
       byte crc = swSer.read();
-      Serial.print("5: ");
-      Serial.println(crc, HEX);
-
-      // byte bytes[4] = {b_0, b_1, OPTIONAL_DATAlen, 0x01};
-
-      // byte crc_calc = CRC8(bytes, 4);
-      // Serial.print("calc crc: ");
-      // Serial.println(crc_calc, HEX);
-      // if (crc != crc_calc) {
-      //  continue;
-      //}
+      DEBUG("5: ");
+      DEBUGln(crc, HEX);
 
       // read the DATA payload
       swSer.readBytes(rx_buffer, DATAlen);
@@ -183,19 +185,4 @@ void loop() {
       swSer.read();
     }
   }
-
-  // while (swSer.available() > 0) {
-  //   swSer.readBytes(rx_buffer, 24);
-  //   // uncomment for datagram debugging
-  //   // for (int i = 0; i < 24; i++) {
-  //   //   Serial.print(i);
-  //   //   Serial.print(": ");
-  //   //   Serial.println(rx_buffer[i], HEX);
-  //   // }
-  //   Serial.println(F("call decoding"));
-  //   if (decodeRxBuffer()) {
-  //     postData();
-  //     Serial.println(F("done uploading"));
-  //   }
-  // }
 }
